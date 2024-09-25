b70d0db15cebe2553499770421a53500a283274e
Дима Архипов <incllude@maclude.local>
2024-09-26 01:46:27 +0300

    little work

--- module.py
+++ module.py
@@ -6,7 +6,6 @@ import torch
 
 
 class StockRankingModel(l.LightningModule):
-
     def __init__(self, sectors, input_size, hidden_size, gat_out_size, num_categories, T):
         super(StockRankingModel, self).__init__()
         self.save_hyperparameters()
@@ -21,50 +20,43 @@ class StockRankingModel(l.LightningModule):
             sector: GAT(hidden_size, gat_out_size) for _ in range(num_categories)
             for sector in sectors
         })
-        
         self.fc = nn.Linear(hidden_size + gat_out_size, 1)
 
-    def sliding_attention(self, gru_output):
+    def sliding_attention(self, gru_embedding):
 
-        batch_size, seq_len, hidden_size = gru_output.shape
         sliding_windows = []
-        gru_output = torch.cat((torch.zeros(batch_size, self.T - 1, hidden_size), gru_output), dim=1)
-        attention_scores = self.attention_layer(gru_output)
+        batch_size, seq_len, hidden_size = gru_embedding.shape
+#         gru_embedding = torch.cat((torch.zeros(batch_size, self.T - 1, hidden_size), gru_embedding), dim=1)
+        scores = torch.vmap(self.attention_layer)(gru_embedding)
         
         for i in range(seq_len - self.W + 1):
-            window = gru_output[:, i:i + self.T, :]
-            window_attention_scores = attention_scores[:, i:i + self.T, :]
-            attention_weights = F.softmax(attention_scores, dim=1)
-            context_vector = torch.sum(attention_weights * window, dim=1)
-            sliding_windows.append(context_vector)
+            window = gru_embedding[:, i : i + self.T, :]
+            window_scores = scores[:, i : i + self.T, :]
+            weights = F.softmax(window_scores, dim=1)
+            attentioned = torch.sum(weights * window, dim=1)
+            sliding_windows.append(attentioned)
         
         sliding_context = torch.stack(sliding_windows, dim=1)
         return sliding_context
     
     def forward(self, x, category):
         
-        category = x.pop("category")        
-        gru_output = [self.gru(x[ticker])[0] for ticker in x]
-        adjacency_matrix = torch.ones(len(x), len(x)) - torch.eye(len(x))
+        category = x.pop("category")
+        nodes_batched = torch.stack(list(x.values()), dim=0)
+        gru_embedding = self.gru(nodes_batched)
         
-        temporal_embedding = torch.vmap(self.sliding_attention)(gru_output)
+        temporal_embedding = self.sliding_attention(gru_embedding)
+        temporal_embedding = temporal_embedding.permute(1, 0, 2)
+        adjacency_matrix = torch.ones(len(x), len(x)) - torch.eye(len(x))
         graph_embedding = self.gat_layers[category](temporal_embedding, adjacency_matrix)
         
-        score = self.fc(combined_embedding)
+        concated_embedding = torch.cat((gru_embedding, graph_embedding), dim=-1)
+        score = self.fc(concated_embedding)
         return score
     
-    def training_step(self, batch, batch_idx):
-        
-        x, edge_index, category, pairwise_labels = batch
-        predictions = self(x, edge_index, category)
-        
-        pos_scores = predictions[pairwise_labels == 1]
-        neg_scores = predictions[pairwise_labels == -1]
-        loss = F.relu(1 - (pos_scores - neg_scores)).mean()
-        
-        self.log('train_loss', loss)
-        return loss
+    def training_step(self, batch):
+        pass
     
     def configure_optimizers(self):
-        return None
+        pass
     
